2380621d524eed5a4d8a6f90093b8913fd85ac97
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-11 00:12:08 -0300

    menu:finish responsiveness

--- src/pages/Menu/styles.ts
+++ src/pages/Menu/styles.ts
@@ -33,6 +33,8 @@ export const SubLayer1 = styled.div`
   flex-direction:column;
   flex:1;
 
+  font-size:1vw;
+
   h4{
     margin-left:auto;
     margin-right:0;
@@ -42,58 +44,58 @@ export const SubLayer1 = styled.div`
     font-family: 'Jura', sans-serif;
     color:var(--content-color);
   }
+  @media(max-width:430px){
+    h4{
+      font-size:3.6vw;
+    }
+  }
 `;
 
 export const TableFood = styled.div`
-font-size:1em;
-width:100%;
-height:100%;
-
-nav {
-  height: 5vh;
-  width: 100%;
-
-  background-color: var(--content-color);
-
-  display: flex;
-  align-items: center;
-  justify-content:center;
-}
-
-nav ul{
-  display:flex;
-  list-style:none;
-
-  li{
-    padding: 0 1em;
-
-    a{
-      text-transform:uppercase;
-      text-decoration:none;
-      color:#2C3E50;
-
-      transition:color 0.2s, border 0.1s;
-      padding:0.5em 0;
-
-      &:hover{
-        color:#B7950B;
-        border-bottom:2px solid;
-      }
-    }
-  }
-}
-  width:96vw;
-  height:96vh;
-
+  width:100%;
+  height:100%;
   background-color:var(--content-color);
 
   display:flex;
   align-items:center;
   justify-content:center;
   flex-direction:column;
-  flex:1;
 
+  font-size:1.2vw;
   font-family: 'Jura', sans-serif;
+
+  nav {
+    height: 5vh;
+
+    background-color: var(--content-color);
+
+    display: flex;
+    align-items: center;
+    justify-content:center;
+  }
+
+  nav ul{
+    display:flex;
+    list-style:none;
+
+    li{
+      padding: 0 1vw;
+
+      a{
+        text-transform:uppercase;
+        text-decoration:none;
+        color:#2C3E50;
+
+        transition:color 0.2s, border 0.1s;
+        padding:0.5vw 0;
+
+        &:hover{
+          color:#B7950B;
+          border-bottom:2px solid;
+        }
+      }
+    }
+  }
 
   table{
     animation: ${appearFromBottom} 1s;
@@ -105,20 +107,20 @@ nav ul{
     font-weight:500;
 
     th{
-      font-size:1.6rem;
+      font-size:1.6vw;
     }
 
     .pasta{
       text-align:center;
       font-weight:700;
-      padding-top:0.5em;
+      padding-top:0.5vw;
       border-top:1px solid;
     }
 
     .price{
       text-align:right;
       border: 1px solid;
-      padding:0.1em;
+      padding:0.1vw;
       width:5vw;
 
       &:hover{
@@ -131,9 +133,20 @@ nav ul{
       margin-right:0;
     }
   }
-  @media(max-width:420px){
-    th{
-      display:none;
+
+  @media(max-width:430px){
+    font-size:4vw;
+
+    nav {
+      width:100%;
+      font-size:4.6vw;
     }
+
+    table{
+      th{
+        font-size:4vw;
+      }
+    }
+
   }
 `;
